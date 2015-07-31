c2d2dcf6d11c55c7d5ac5dc855e710e0f5bb3a56
Rafayel Mkrtchyan <rafamian@berkeley.edu>
2015-07-31 00:02:55 -0700

    Update configureHook.sh

--- externals/nghttp2/src/configureHook.sh
+++ externals/nghttp2/src/configureHook.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
 
 sh configure LDFLAGS="$LDFLAGS -rdynamic" CPPFLAGS="$CPPFLAGS -D_FILE_OFFSET_BITS=64" CFLAGS="$CFLAGS -fno-strict-aliasing -fasynchronous-unwind-tables -fno-omit-frame-pointer -fno-optimize-sibling-calls -fvisibility=hidden -fPIC"
+ --disable-option-checking
+ --disable-FEATURE
+ --enable-FEATURE[=ARG] 
+ --enable-shared[=PKGS]
+ --enable-static[=PKGS]
+ --enable-fast-install[=PKGS]
+ --disable-libtool-lock
+ --enable-dependency-tracking
+ --disable-dependency-tracking
+ --enable-silent-rules
+ --disable-silent-rules
+ --enable-werror
+ --enable-debug 
+ --disable-threads  
+ --enable-app          
+ --enable-hpack-tools
+ --enable-asio-lib 
+ --enable-examples
+ --enable-python-bindings
+ --disable-failmalloc
+ --disable-xmltest
+ --disable-assert 
+ --disable-largefile    
+
  
