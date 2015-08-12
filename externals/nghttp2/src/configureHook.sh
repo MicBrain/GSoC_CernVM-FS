fff9184423aaa2b7ce743902285ade9fd7476613
Rafayel Mkrtchyan <rafamian@berkeley.edu>
2015-08-12 01:32:09 -0700

    Update configureHook.sh

--- externals/nghttp2/src/configureHook.sh
+++ externals/nghttp2/src/configureHook.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 sh ./configure .... \
- --enable-static
- --disable-shared
- --disable-python-bindings
- --disable-failmalloc
- --disable-xmltest
- --disable-app
- --disable-hpack-tools
- --disable-asio
+ --enable-static \
+ --disable-shared \
+ --disable-python-bindings \
+ --disable-failmalloc \
+ --disable-xmltest \
+ --disable-app \
+ --disable-hpack-tools \
+ --disable-asio \
  --disable-examples
