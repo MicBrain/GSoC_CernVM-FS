1eb8af48ba5c930d7f6d6df6fa32b97beb31552b
Rafayel Mkrtchyan <rafamian@berkeley.edu>
2015-08-12 01:19:25 -0700

    Update configureHook.sh

--- externals/nghttp2/src/configureHook.sh
+++ externals/nghttp2/src/configureHook.sh
@@ -3,15 +3,6 @@
 sh ./configure .... \
  --enable-static
  --disable-shared
- --disable-FEATURE
- --disable-libtool-lock
- --enable-dependency-tracking
- --disable-dependency-tracking
- --enable-silent-rules
- --disable-silent-rules
- --enable-werror
- --enable-debug 
- --disable-threads
  --enable-python-bindings
  --disable-failmalloc
  --disable-xmltest
